0c18fbaf1f7aad09cb23f39c782603dd071522c4
BaatilRhyme <90560421Aq/>
2014-06-19 08:04:08 +0300

    test

---
+++ BedelliAskerlikTweeter/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedelliAskerlikTweeter
+{
+   public class Util
+    {
+       public static string GetPost(string post, string additionalString)
+       {
+           string p = string.Empty;
+
+           if (!string.IsNullOrEmpty(additionalString))
+           {
+               if (140 - post.Length > additionalString.Length + 1)
+                   p = post + " " + additionalString;
+               else
+                   p = post.Substring(0, post.Length - additionalString.Length + 1) + " " + additionalString;
+           }
+           return p;
+       }
+    }
+}
